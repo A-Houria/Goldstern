1614ee5fbc209a025cecf1364b2b24b1435c8a21
abdelrahman-houria <amr.houria70@gmail.com>
2025-02-25 00:51:10 +0100

    fixed handlechange in services

--- src/Services.js
+++ src/Services.js
@@ -14,6 +14,10 @@ const Services = () => {
     AOS.init({ duration: 1000 });
   }, []);
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
