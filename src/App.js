3e539f14ff141d3f6e60dc29761287c787abf2dd
abdelrahman-houria <amr.houria70@gmail.com>
2025-06-04 20:46:45 +0200

    removed login page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import {
 import Layout from "./Layout";
 import Home from "./Home";
 import About from "./About";
-import Login from "./Login";
 import Inventory from "./Inventory";
 import Services from "./Services";
 import Contact from "./Contact";
@@ -23,7 +22,6 @@ const router = createBrowserRouter(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/login" element={<Login />} />
       <Route path="/inventory" element={<Inventory />} />
       <Route path="/services" element={<Services />} />
       <Route path="/contact" element={<Contact />} />
